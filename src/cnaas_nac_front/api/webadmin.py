ece097f292d6d3e7694988ec60bef3936fbd5c62
Kristofer Hallin <kristofer@sunet.se>
2020-02-19 09:32:43 +0100

    Added port bounce. Will get the hostname from NMS API and use it to bounce the selected port.

--- src/cnaas_nac_front/api/webadmin.py
+++ src/cnaas_nac_front/api/webadmin.py
@@ -7,6 +7,7 @@
 import os
 import requests
 
+from flask import flash
 from flask import request
 from flask import redirect
 from flask import render_template
@@ -28,9 +29,12 @@ class UserForm(FlaskForm):
     disable = SubmitField('Disable user(s)')
     set_vlan_btn = SubmitField('Set VLAN')
     set_vlan = StringField()
+    bounce_btn = SubmitField('Bounce port(s)')
 
 
-def get_users(api_url, token):
+def get_users(api_url, token, username=''):
+    if username != '':
+        api_url += '/{}'.format(username)
     try:
         res = requests.get(api_url,
                            headers={'Authorization': 'Bearer ' + token},
@@ -58,7 +62,6 @@ def add_user(api_url, token, username, password, vlan):
                             verify=False)
         json = res.json()
     except Exception as e:
-        print(str(e))
         return {}
 
     if 'data' in json:
@@ -139,16 +142,76 @@ def get_jwt_token():
 
 def get_api_url():
     try:
-        token = os.environ['AUTH_API_URL']
+        url = os.environ['AUTH_API_URL']
     except Exception:
         raise ValueError('Could not find API URL')
-    return token
+    return url
+
+
+def get_nms_url():
+    try:
+        url = os.environ['NMS_API_URL']
+    except Exception:
+        raise ValueError('Could not find NMS API URL')
+    return url
+
+
+def bounce_port(api_url, nms_url, token, username):
+    user_data = get_users(api_url, token, username=username)
+
+    if username not in user_data:
+        return 'Invalid response from auth API'
+    if 'nas_ip_address' not in user_data[username]:
+        return 'Could not find switch IP address'
+    if 'nas_port_id' not in user_data[username]:
+        return 'Could not find switch port'
+
+    nas_ip_address = user_data[username]['nas_ip_address']
+    nas_port_id = user_data[username]['nas_port_id']
+
+    try:
+        url = nms_url
+        url += 'devices?filter[management.ip]={}'.format(nas_ip_address)
+        res = requests.get(url, headers={'Authorization': 'Bearer ' + token},
+                           verify=False)
+        json = res.json()
+
+        if json['data']['devices'] == []:
+            return 'Could not find switch management IP'
+        if 'hostname' not in json['data']['devices'][0]:
+            return 'Could not find switch hostname'
+
+    except Exception as e:
+        return 'Could not get management IP: ' + str(e)
+
+    hostname = json['data']['devices'][0]['hostname']
+
+    try:
+        url = nms_url + 'device/{}/interface_status'.format(hostname)
+        res = requests.put(url, headers={'Authorization': 'Bearer ' + token},
+                           verify=False,
+                           json={'bounce_interfaces': [nas_port_id]})
+        json = res.json()
+
+        if json['status'] == 'error':
+            message = json['message']
+        else:
+            message = json['data']
+
+        if res.status_code != 200:
+            return 'Failed to bounce port: ' + message
+
+    except Exception as e:
+        return 'Failed to bounce port: ' + str(e)
+
+    return 'Port bounced: ' + message
 
 
 class WebAdmin(Resource):
     @classmethod
     def index(cls):
         api_url = get_api_url()
+        nms_url = get_nms_url()
         token = get_jwt_token()
         users = get_users(api_url, token)
         form = UserForm()
@@ -174,6 +237,16 @@ class WebAdmin(Resource):
                 selected = request.form.getlist('selected')
                 for user in selected:
                     disable_user(api_url, token, user)
+            elif 'bounce_btn' in result:
+                selected = request.form.getlist('selected')
+                if len(selected) > 1:
+                    flash('You can only bounce one port.')
+                elif len(selected) == 0:
+                    flash('Please select a port to bounce.')
+                else:
+                    for user in selected:
+                        message = bounce_port(api_url, nms_url, token, user)
+                        flash('Port bounce: ' + message)
             elif 'set_vlan_btn' in result:
                 set_vlan = result['set_vlan']
                 if set_vlan != "" and set_vlan:
