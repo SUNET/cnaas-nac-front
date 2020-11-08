9c3f287bc4f77799b99c3c72d9cc61b68195744f
Kristofer Hallin <kristofer@sunet.se>
2020-11-08 19:55:21 +0100

    Component for modal windows.

---
+++ public/components/Modal.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./modal.css";
+import PropTypes from "prop-types";
+
+export default class Modal extends React.Component {
+    onClose = e => {
+	this.props.onClose && this.props.onClose(e);
+    };
+
+    onSubmit = e => {
+	this.props.onSubmit && this.props.onSubmit(e);
+	this.props.onClose && this.props.onClose(e);
+    };
+
+    render() {
+	if (!this.props.show) {
+	    return null;
+	}
+	return (
+	    <div class="modal" id="modal">
+		<div class="content">{this.props.children}</div>
+		<div class="actions">
+		    <button onClick={this.onSubmit}>OK</button>
+		    <button onClick={this.onClose}>Cancel</button>		    
+		</div>
+	    </div>
+	);
+    }
+}
+Modal.propTypes = {
+    onSubmit: PropTypes.func.isRequired,
+    onClose: PropTypes.func.isRequired,
+    show: PropTypes.bool.isRequired
+};
