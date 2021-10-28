2edccede2fd03b89724bdcf67bc461a48396927e
Ernst Widerberg <ernst@sunet.se>
2021-10-28 09:41:53 +0200

    Move btoaUTF16 to LoginForm

--- public/components/LoginForm.js
+++ public/components/LoginForm.js
@@ -2,6 +2,18 @@ import React from "react";
 
 import checkResponseStatus from "../utils/checkResponseStatus";
 
+// NOTE: Currently not used
+// passible base64 encode function?
+function btoaUTF16(sString) {
+    var aUTF16CodeUnits = new Uint16Array(sString.length);
+    Array.prototype.forEach.call(aUTF16CodeUnits, function (el, idx, arr) {
+        arr[idx] = sString.charCodeAt(idx);
+    });
+    return btoa(
+        String.fromCharCode.apply(null, new Uint8Array(aUTF16CodeUnits.buffer))
+    );
+}
+
 class LoginForm extends React.Component {
     state = {
         email: "",
--- public/components/Panel.js
+++ public/components/Panel.js
@@ -5,17 +5,6 @@ import { Route, Switch } from "react-router-dom";
 import { postData } from "react-router-dom";
 import { Button } from "semantic-ui-react";
 
-// passible base64 encode function?
-function btoaUTF16(sString) {
-    var aUTF16CodeUnits = new Uint16Array(sString.length);
-    Array.prototype.forEach.call(aUTF16CodeUnits, function (el, idx, arr) {
-        arr[idx] = sString.charCodeAt(idx);
-    });
-    return btoa(
-        String.fromCharCode.apply(null, new Uint8Array(aUTF16CodeUnits.buffer))
-    );
-}
-
 class Panel extends React.Component {
     render() {
         console.log("this is props (in panel)", this.props);
