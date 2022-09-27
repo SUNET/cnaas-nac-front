7871a5e3e213d528888f754582dbbebf2f6da0f0
Kristofer Hallin <kristofer@sunet.se>
2022-09-27 15:01:16 +0200

    Proper indentation.

--- public/components/Header.js
+++ public/components/Header.js
@@ -2,25 +2,25 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 class Header extends React.Component {
-  render() {
-    return (
-      <header>
-	<nav>
-	  <ul>
-	    <NavLink exact activeClassName="active" to={`/`}>
-	      <li>Start</li>
-	    </NavLink>
-	    <NavLink exact activeClassName="active" to={`/clients`}>
-	      <li>Clients</li>
-	    </NavLink>
-	    <NavLink exact activeClassName="active" to={`/groups`}>
-	      <li>Groups</li>
-	    </NavLink>
-	  </ul>
-	</nav>
-      </header>
-    );
-  }
+    render() {
+	return (
+	    <header>
+		<nav>
+		    <ul>
+			<NavLink exact activeClassName="active" to={`/`}>
+			    <li>Start</li>
+			</NavLink>
+			<NavLink exact activeClassName="active" to={`/clients`}>
+			    <li>Clients</li>
+			</NavLink>
+			<NavLink exact activeClassName="active" to={`/groups`}>
+			    <li>Groups</li>
+			</NavLink>
+		    </ul>
+		</nav>
+	    </header>
+	);
+    }
 }
 
 export default Header;
