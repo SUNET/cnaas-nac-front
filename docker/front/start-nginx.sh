8c0b1b1c7f60e97c0298112e53473f259deb2413
Kristofer Hallin <kristofer@sunet.se>
2025-03-24 09:59:41 +0000

    Load certificates from certs/ and not cert/.

--- docker/front/start-nginx.sh
+++ docker/front/start-nginx.sh
@@ -2,8 +2,8 @@
 
 sed -e "s|^\(.*proxy_pass \)CNAAS_API_URL;$|\1$CNAAS_API_URL;|" \
     -e "s|^\(.*proxy_pass \)CNAAS_AUTH_URL;$|\1$CNAAS_AUTH_URL;|" \
-  < /etc/nginx/sites-available/nginx_app.conf > /tmp/nginx_app.conf.new \
-  && cat /tmp/nginx_app.conf.new > /etc/nginx/sites-available/nginx_app.conf
+    < /etc/nginx/sites-available/nginx_app.conf > /tmp/nginx_app.conf.new \
+    && cat /tmp/nginx_app.conf.new > /etc/nginx/sites-available/nginx_app.conf
 
 sed -e "s|^\(.*API_URL=\)CNAAS_FRONT_URL$|\1$CNAAS_FRONT_URL|" \
   < /opt/cnaas/.env > /tmp/.env \
@@ -13,12 +13,13 @@ cd /opt/cnaas/cnaas-nac-front
 npm run-script build
 cp dist/* /opt/cnaas/static
 
-if [ ! -f /opt/cnaas/cert/cnaasfront_combined.crt ]; then
+if [ ! -f /opt/cnaas/certs/cnaasfront_combined.crt ]; then
   echo "WARNING: no cert found, using snakeoil cert"
-  cp /etc/nginx/conf.d/snakeoil.crt /opt/cnaas/cert/cnaasfront_combined.crt
-  cp /etc/nginx/conf.d/snakeoil.key /opt/cnaas/cert/cnaasfront.key
+  cp /etc/nginx/conf.d/snakeoil.crt /opt/cnaas/certs/cnaasfront_combined.crt
+  cp /etc/nginx/conf.d/snakeoil.key /opt/cnaas/certs/cnaasfront.key
 fi
-chown root:root /opt/cnaas/cert/cnaasfront.key
-chmod 600 /opt/cnaas/cert/cnaasfront.key
+
+chown root:root /opt/cnaas/certs/cnaasfront.key
+chmod 600 /opt/cnaas/certs/cnaasfront.key
 
 /usr/sbin/nginx -g "daemon off;"
